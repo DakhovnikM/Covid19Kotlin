7fc33c7b34d065f2ba8e03397354e3e007e5806c
dakhovnikAnna <dahovnikanna@gmail.com>
2021-11-22 21:46:02 +0200

    Переписана разметка activity_main

--- app/src/main/java/com/example/covid19kotlin/MainActivity.kt
+++ app/src/main/java/com/example/covid19kotlin/MainActivity.kt
@@ -9,9 +9,9 @@ import com.example.covid19kotlin.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     //private lateinit var countriesListView: ListView
-    lateinit var bindingActivityMain: ActivityMainBinding
 
-    var countries : Array<String> = arrayOf("")
+    lateinit var bindingActivityMain: ActivityMainBinding
+    var countries: Array<String> = arrayOf("")
     val count: String = "Привет как дела"
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,9 +22,15 @@ class MainActivity : AppCompatActivity() {
 
         //setContentView(R.layout.activity_main)
         //countriesListView = findViewById(R.id.countriesList)
+
+        bindingActivityMain.btn1.setOnClickListener() {
+            countries = arrayOf("Бразилия", "Аргентина", "Колумбия", "Чили", "Уругвай")
+            var adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, countries)
+            bindingActivityMain.countriesList.adapter = adapter
+        }
     }
 
-    fun updateClick(view: android.view.View) {
+    /*fun updateClick(view: android.view.View) {
         countries = arrayOf("Бразилия", "Аргентина", "Колумбия", "Чили", "Уругвай")
 
         var adapter = ArrayAdapter(
@@ -32,9 +38,9 @@ class MainActivity : AppCompatActivity() {
             android.R.layout.simple_list_item_1, countries
         )
 
-        bindingActivityMain.countriesList.adapter= adapter
+        bindingActivityMain.countriesList.adapter = adapter
         //countriesListView.adapter = adapter
-    }
+    }*/
 
 
 }
